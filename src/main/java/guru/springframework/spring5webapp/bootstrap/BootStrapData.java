19920caf1107155affda516cac8a7555863c2488
andrei.gaman <andrei.gaman994@gmail.com>
2021-05-06 09:10:42 +0300

    Entities

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book book1 = new Book("Metro", "123");
+
+        eric.getBooks().add(book1);
+        book1.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(book1);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE", "12313123");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in BootStrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
+        Publisher pub = new Publisher("Alex", "address", "Onesti", "607220");
+        publisherRepository.save(pub);
+        System.out.println("PUBLISHER " + pub.toString());
+
+    }
+}
